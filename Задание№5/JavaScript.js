c8174e1c37027f74068de5f96783766fa7f8ca43
MrOganes <112868498+MrOganes@users.noreply.github.com>
2022-10-08 09:17:36 +0300

    Update JavaScript.js

--- Задание№5/JavaScript.js
+++ Задание№5/JavaScript.js
@@ -1,32 +1,32 @@
+/*jslint browser */
+/*jslint devel */
+
 function onClick() {
-  let x, y, r;
-  x=document.getElementById("kol").value;
-  y=document.getElementById("price").value;
-  r=document.getElementById("result")
-  x=parseInt(x);
-  y=parseInt(y);
-  r.innerHTML=x*y;
-  return false;
+    let x = document.getElementById("kol").value;
+    let y = document.getElementById("price").value;
+    let r = document.getElementById("result");
+    x = parseInt(x);
+    y = parseInt(y);
+    r.innerHTML = x * y;
+    return false;
 }
 
-function prov(){
-  let a,b,ex, clear;
-  clear=document.getElementById("result");
-  clear.innerHTML="";
-  a=document.getElementById("kol").value;
-  b=document.getElementById("price").value;
-  ex=/\d+/;
-  a=ex.test(a);
-  b=ex.test(b);
-  if(a==true && b==true){
-    onClick();
-  }
-  else{
-    alert("Введенные данные некорректны!")
-  }
+function prov() {
+    let clear = document.getElementById("result");
+    clear.innerHTML = "";
+    let a = document.getElementById("kol").value;
+    let b = document.getElementById("price").value;
+    let ex = /\d+/;
+    a = ex.test(a);
+    b = ex.test(b);
+    if (a === true && b === true) {
+        onClick();
+    } else {
+        alert("Введенные данные некорректны!");
+    }
 }
 
-window.addEventListener('DOMContentLoaded', function(){
-  let c=document.getElementById("my-button");
-  c.addEventListener("click", prov);
+window.addEventListener("DOMContentLoaded", function () {
+    let c = document.getElementById("my-button");
+    c.addEventListener("click", prov);
 });
