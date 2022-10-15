fd407c3eac5a6da3077d1a8a8496b994bd300796
MrOganes <112868498+MrOganes@users.noreply.github.com>
2022-10-15 13:15:08 +0300

    Update JavaScript3.js

--- №7/JavaScript3.js
+++ №7/JavaScript3.js
@@ -3,23 +3,23 @@
 /*jslint devel */
 
 jQuery(function ($) {
-  $(document).ready(function () {
-      $(".slider").slick({
-          arrows: true,
-          dots: true,
-          responsive: [
-              {
-                  breakpoint: 560,
-                  settings: {
-                      arrows: true,
-                      dots: true,
-                      slidesToScroll: 1,
-                      slidesToShow: 2
-                  }
-              }
-          ],
-          slidesToScroll: 1,
-          slidesToShow: 4
-      });
-  });
+    $(document).ready(function () {
+        $(".slider").slick({
+            arrows: true,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 560,
+                    settings: {
+                        arrows: true,
+                        dots: true,
+                        slidesToScroll: 1,
+                        slidesToShow: 2
+                    }
+                }
+            ],
+            slidesToScroll: 1,
+            slidesToShow: 4
+        });
+    });
 });
